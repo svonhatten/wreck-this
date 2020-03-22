78fbf276904c2215b37f193a08667edcd159d275
svonhatten <svonhatten@luc.edu>
2020-03-21 21:40:18 -0500

    mostly completed home page

--- wreck-this/src/WebPages/Home/Home.jsx
+++ wreck-this/src/WebPages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-// import { homeStyles } from './styles.js';
 import './styles.css';
+import bubble from './bubble.jpg';
+import rip from './rip.jpg';
+import explosion from './explosion.jpg';
 
 class Home extends Component {
     render() {
@@ -13,15 +15,57 @@ class Home extends Component {
                         <div className={'navBarLogo'}>logo</div>
                         <div className={'navBarText'}>WreckIt</div>
                         {/* container for destruction options on right side */}
+                        <div className={'navBarDestr'}>
+                            <div className={'navBarDestrLogo'}>
+                                <img src={bubble} alt="bubble"/>
+                            </div>
+                            <div className={'navBarDestrText'}>Bubbles</div>
+                            <div className={'navBarDestrLogo'}>
+                                <img src={rip} alt="rip"/>
+                            </div>
+                            <div className={'navBarDestrText'}>Rip</div>
+                            <div className={'navBarDestrLogo'}>
+                                <img src={explosion} alt="explosion"/>
+                            </div>
+                            <div className={'navBarDestrText'}>Explosion</div>
+                        </div>
                     </div>
                     {/* title section (the black part of screen) */}
                     <div className={'titleContainer'}>
                         <div className={'titleText'}>Wreck This Website</div>
-                        <div className={'titleSubtext'}>* * * The site to relieve all your stress * * *</div>
+                        <div className={'titleSubtext'}>*   *   *   The site to relieve all your stress   *   *   *</div>
                     </div>
                     {/* bottom section (everything below title section) */}
                     <div className={'bodyContainer'}>
-                        Destruction Choices
+                        <div className={'bodyTitleText'}>Choose Your Path</div>
+                        <div className={'bodyTitleSubtext'}>Pick the option that most closely relates to how you're feeling</div>
+                        {/* destruction choices */}
+                        <div className={'destrChoicesContainer'}>
+                            {/* Bubbles */}
+                            <div className={'bubbleContainer'}>
+                                <div className={'bubbleImage'}>
+                                    Image goes here
+                                </div>
+                                <div className={'bubbleText'}>Bubbles</div>
+                                <div className={'bubbleSubtext'}>Bubble out that which you hate the most!</div>
+                            </div>
+                            {/* Tearing */}
+                            <div className={'ripContainer'}>
+                                <div className={'ripImage'}>
+                                    Image goes here
+                                </div>
+                                <div className={'ripText'}>Rip</div>
+                                <div className={'ripSubtext'}>Rip the website to shreds!</div>
+                            </div>
+                            {/* Explosion */}
+                            <div className={'explosionContainer'}>
+                                <div className={'explosionImage'}>
+                                    Image goes here
+                                </div>
+                                <div className={'explosionText'}>EXPLOSIONS!</div>
+                                <div className={'explosionSubtext'}>Make this website go out with a BANG!</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
